5a294cee678d3c0520daadc5d75971e48398b6e5
pjdufour <pjdufour.dev@gmail.com>
2019-03-17 15:52:58 -0400

    add copyright

--- main.go
+++ main.go
@@ -1,3 +1,10 @@
+// =================================================================
+//
+// Copyright (C) 2019 Spatial Current, Inc. - All Rights Reserved
+// Released as open source under the MIT License.  See LICENSE file.
+//
+// =================================================================
+
 package main
 
 import (
